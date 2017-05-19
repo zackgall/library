c63ae2b3add1886d75b8986311c56987e362fb51
Zack <zackgall@gmail.com>
2017-05-19 21:29:33 +0000

    setup gulp and bower

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.use(express.static('src/views/'));
+
+app.get('/', function(req, res){
+    res.send('hello world');
+    });
+    
+    app.get('/books', function(req, res){
+    res.send('hello books');
+    });
+    
+    
+app.listen(port, function(err){
+    console.log('running server on ' + port);
+});
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+
+var jsFiles = ['*.js', 'src/**/*.s'];
+
+
+gulp.task('style', function(){
+    return gulp.src(jsFiles)   
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs());
+});
