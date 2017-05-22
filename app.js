6b74bd48f12e4cb63708db1b75f9d3f458bcdbed
Zack <zackgall@gmail.com>
2017-05-22 18:24:49 +0000

    installed more dependencies and setup more gulp configurations

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3000;
+
 app.use(express.static('public'));
-app.use(express.static('src/views/'));
+app.set('views', 'src/views');
+
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.send('hello world');
+    res.render('index', {title: 'hello from render', list: ['a', 'b']});
     });
     
     app.get('/books', function(req, res){
